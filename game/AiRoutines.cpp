368dc86da278735033c6eaf1f0ac31567d3d611e
Jordan Savant <md5madman@gmail.com>
2014-05-22 13:35:50 -0500

    Smarter ai will move to you and then attack

--- game/AiRoutines.cpp
+++ game/AiRoutines.cpp
@@ -3,19 +3,58 @@
 #include "Level.hpp"
 #include "Tile.hpp"
 #include "../bitengine/Intelligence.hpp"
+#include "../bitengine/Math.hpp"
 
 void AiRoutines::Combat::DecideZombie(Character* character)
 {
-    bool complete = false;
-    character->inspectVisibleCharacters([character, &complete] (Character* occupant) {
-        if(complete)
-            return;
-        // If the tile has a character that is not my own, choose them as my enemy and attack
-        character->combat_DecideAction_AttackCharacter(occupant);
-        complete = true;
+    // Find the closest enemy
+    Character* closestEnemy = NULL;
+    float closestDistance = 0;
+    character->inspectVisibleCharacters([character, &closestEnemy, &closestDistance] (Character* occupant) {
+        int x1 = character->Body::schema.x;
+        int y1 = character->Body::schema.y;
+        int width1 = character->Body::schema.width;
+        int height1 = character->Body::schema.height;
+        int x2 = occupant->Body::schema.x;
+        int y2 = occupant->Body::schema.y;
+        int width2 = occupant->Body::schema.width;
+        int height2 = occupant->Body::schema.height;
+
+        // If the occupant is not adjacent to me then move towards them
+        float distance = bit::RectangleMath::distance(x1, y1, width1, height1, x2, y2, width2, height2);
+        if(closestEnemy == NULL || distance < closestDistance)
+        {
+            closestEnemy = occupant;
+            closestDistance = distance;
+        }
     });
 
-    // Lets decide we want to move 2 tiles north
-    if(!complete)
+    // If I have an enemy
+    if(closestEnemy)
+    {
+        int x1 = character->Body::schema.x;
+        int y1 = character->Body::schema.y;
+        int width1 = character->Body::schema.width;
+        int height1 = character->Body::schema.height;
+        int x2 = closestEnemy->Body::schema.x;
+        int y2 = closestEnemy->Body::schema.y;
+        int width2 = closestEnemy->Body::schema.width;
+        int height2 = closestEnemy->Body::schema.height;
+
+        if(bit::RectangleMath::isCardinallyAdjacent(x1, y1, width1, height1, x2, y2, width2, height2))
+        {
+            // If I am directly next to them, attack them
+            character->combat_DecideAction_AttackCharacter(closestEnemy);
+        }
+        else
+        {
+            // If they are not next to me then move towards them (DEBUG MOVE ONE TILE ABOVE)
+            character->combat_DecideAction_MoveToLocation(closestEnemy->Body::schema.x, closestEnemy->Body::schema.y - character->level->tileHeight);
+        }
+    }
+    // If I have no enemy, wander north
+    else
+    {
         character->combat_DecideAction_MoveToLocation(character->Body::schema.x, character->Body::schema.y - character->level->tileHeight * 2);
+    }
 }
